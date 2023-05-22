aaa7687d26c818dd5f70958393071877d5e8c9ee
Alex <111201814+Alex666777666@users.noreply.github.com>
2023-05-22 14:02:39 +0300

    Update ConnectionStatus.jsx

--- src/ConnectionStatus.jsx
+++ src/ConnectionStatus.jsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const ConnectionStatus = () => {
-  const [status, setStatus] = useState('online')
+  const [status, setStatus] = useState('online');
 
   useEffect(() => {
     const handleOnline = () => {
-      setStatus('online')
-    }
+      setStatus('online');
+    };
 
     const handleOffline = () => {
-      setStatus('offline')
-    }
+      setStatus('offline');
+    };
 
-    window.addEventListener('online', handleOnline)
-    window.addEventListener('offline', handleOffline)
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
 
     return () => {
-      window.removeEventListener('online', handleOnline)
-      window.removeEventListener('offline', handleOffline)
-    }
-  }, [status])
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, [status]);
 
   return (
-    <div className={status ? 'status' : 'status status_offline'}>
-      {status ? 'online' : 'offline'}
+    <div className={`status ${status === 'offline' ? 'status_offline' : null}`}>
+      {status === 'online' ? 'online' : 'offline'}
     </div>
-  )
-}
+  );
+};
 
-export default ConnectionStatus
+export default ConnectionStatus;
